7eb3ea203144317cdc5e02cbab75a7ea77628c95
Jean-Simon <jeansimonhoule@hotmail.com>
2020-10-16 11:42:44 -0400

    Data saved in folders

--- sensor.py
+++ sensor.py
@@ -1,6 +1,10 @@
 import serial
 import time
 import csv
+import datetime
+import os
+from pathlib import Path
+
 
 
 class Accelerometer:
@@ -16,13 +20,30 @@ class Accelerometer:
     def getAcceleration(self):
         """Reads and decode to string the information from the serial port"""
         self.acceleration = self.port.readline().decode("utf-8").replace(" \r\n",'').split(',')
-        return self.acceleration
-    
-    def write_to_csv(self, filename):
+
+
+    def create_folder(self):
+        #get date of capture
+        date = datetime.datetime.now().strftime("%Y_%m_%d")
+
+        #create the folder named after the day of capture
+        path = Path(os.path.abspath(__file__))
+        path = path.parent.joinpath("SAVED_DATA").joinpath(str(date))
+        if path.exists() == False:
+            os.mkdir(path)
+        return path
+        
+    def write_to_csv(self, filename_data,filename_reference):
         """Writes data to csv file"""
-        with open ('test_csv.csv','a') as file:
-            writer = csv.writer(file)
-            writer.writerow(self.acceleration)
+        if int(self.acceleration[0]) == 1:
+            with open (str(self.create_folder().joinpath('REFERENCE.csv')),'a',newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(self.acceleration[1:])
+        else:
+            with open (str(self.create_folder().joinpath('DATA.csv')),'a',newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(self.acceleration[1:])
+
 
         
 def main():
@@ -30,7 +51,9 @@ def main():
     sensor.setPort('COM4',9600)
     while True:
         sensor.getAcceleration()
-        sensor.write_to_csv("test_csv.csv")
+        print(sensor.acceleration)
+        sensor.write_to_csv("DATA.csv","REFERENCE.csv")
+
 
 if __name__ == "__main__":
     main()
---
+++ testdate.py
@@ -0,0 +1,5 @@
+import datetime
+
+x = datetime.datetime.now()
+x = x.strftime("%Y_%m_%d")
+print(x)
